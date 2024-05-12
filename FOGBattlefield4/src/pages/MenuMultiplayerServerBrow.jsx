70c039ba9a691d4f6a5389301ef0909fbe4a0066
Prashasti2004 <105813095+Prashasti2004@users.noreply.github.com>
2024-05-12 21:03:01 +0530

    Add files via upload

---
+++ FOGBattlefield4/src/pages/MenuMultiplayerServerBrow.jsx
@@ -0,0 +1,373 @@
+import FrameComponent1 from "../components/FrameComponent1";
+import Invite from "../components/Invite";
+import FrameComponent from "../components/FrameComponent";
+import "./MenuMultiplayerServerBrow.css";
+
+const MenuMultiplayerServerBrow = () => {
+  return (
+    <div className="menu-multiplayer-server-brow menu-background-icon">
+      {/* <div className="wrapper-menu-background">
+        <img className="menu-background-icon" src="/bgimage.png" alt="" />
+      </div> */}
+      <div className="output-aggregator" />
+      <div className="frame-parent">
+        <div className="frame-group">
+          <FrameComponent1 />
+          <div className="frame-wrapper">
+            <div className="frame-container">
+              <div className="browser-breadcrumb-wrapper">
+                <div className="browser-breadcrumb">
+                  <div className="frame-div">
+                    <div className="frame-parent1">
+                      <div className="back-wrapper">
+                        <img
+                          className="back-icon"
+                          loading="lazy"
+                          alt=""
+                          src="/back.svg"
+                        />
+                      </div>
+                      <nav className="frame-nav">
+                        <div className="multiplayer-wrapper">
+                          <div className="multiplayer">multiplayer</div>
+                        </div>
+                        <h3 className="h3">/</h3>
+                        <div className="server-browser-wrapper">
+                          <div className="server-browser">server browser</div>
+                        </div>
+                      </nav>
+                    </div>
+                    <div className="server-info-wrapper">
+                      <h1 className="server-info">server info</h1>
+                    </div>
+                  </div>
+                  <h3 className="h31">/</h3>
+                </div>
+              </div>
+              <div className="frame-parent2">
+                <div className="nasa-noobs-welcome-conques-parent">
+                  <h1 className="nasa-noobs">
+                    !RC3-BASECAMPS
+                  </h1>
+                  <div className="frame-parent3">
+                    <div className="conquest-large-siege-of-shan-wrapper">
+                      <div className="conquest-large-">
+                      CONQUEST LARGE-LANCANG DAM-CUSTOM-60HZ
+                      </div>
+                    </div>
+                    <div className="server-protected-by-container">
+                      <p className="server-protected-by">
+                        Server protected by The_K-50 AntiCheat. Vip !Rules,
+                        Questions, Request, Appeal, Visit us: www.epg.gg |
+                        Discord
+                      </p>
+                      <p className="httpsdiscordgg3r5nk4d">
+                        https://discord.gg/3r5NK4d
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="frame-parent4">
+                  <div className="frame-parent5">
+                    <div className="browser-si-buttons-wrapper">
+                      <button className="browser-si-buttons">
+                        <div className="join">join</div>
+                        <div className="browser-si-buttons-child" />
+                      </button>
+                    </div>
+                    <div className="frame-parent6">
+                      <div className="players-parent">
+                        <div className="players">players</div>
+                        <div className="wrapper">
+                          <div className="div">64/64</div>
+                        </div>
+                      </div>
+                      <div className="data-splinters">
+                        <div className="ping">ping</div>
+                        <div className="ms-wrapper">
+                          <div className="ms">47ms</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="browser-si-buttons-parent">
+                    <button className="browser-si-buttons1">
+                      <div className="spectate">spectate</div>
+                      <div className="browser-si-buttons-item" />
+                    </button>
+                    <div className="audio-parser">
+                      <div className="connection-handler">
+                        <div className="tickrate">tickrate</div>
+                        <div className="hz">60 Hz</div>
+                      </div>
+                    </div>
+                  </div>
+                  <button className="browser-si-buttons2">
+                    <div className="join-as-commander">join as commander</div>
+                    <div className="browser-si-buttons-inner" />
+                  </button>
+                  <div className="browser-si-buttons3">
+                    <div className="star-shape" />
+                    <img
+                      className="favorite-icon"
+                      loading="lazy"
+                      alt=""
+                      src="/favorite.svg"
+                    />
+                    <div className="div1">13672</div>
+                  </div>
+                </div>
+                <div className="xor-gate">
+                  <div className="frame-parent7">
+                    <div className="settings-wrapper">
+                      <div className="settings">settings</div>
+                    </div>
+                    <div className="advanced">advanced</div>
+                    <div className="rules">rules</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="invite-wrapper-pannel">
+        <div className="invite-wrapper">
+          <Invite />
+        </div>
+        </div>
+      </div>
+      <div className="data-processor">
+        <div className="frame-parent8">
+          <div className="side-menu-wrapper">
+            <div className="side-menu">
+              <div className="side-menu-news">
+                <div className="side-menu-news-child" />
+                <div className="side-menu-news-item" />
+                <div className="side-menu-news-inner" />
+              </div>
+            </div>
+          </div>
+          <div className="frame-parent9">
+            <div className="browser-si-region-w-parent">
+              <div className="browser-si-region-w">
+                <div className="browser-si-region-wrapper">
+                  <div className="browser-si-region">
+                    <div className="region">Region</div>
+                    <div className="europe-de">europe - de</div>
+                  </div>
+                </div>
+                <div className="browser-si-region-w-child" />
+              </div>
+              <div className="data-aggregator">
+                <div className="shape-collection">
+                  <div className="minimap-parent">
+                    <div className="minimap">minimap</div>
+                    <div className="on">on</div>
+                  </div>
+                </div>
+                <div className="data-aggregator-child" />
+              </div>
+              <div className="data-aggregator1">
+                <div className="data-aggregator-inner">
+                  <div className="tickets-parent">
+                    <div className="tickets">tickets</div>
+                    <div className="div2">400</div>
+                  </div>
+                </div>
+                <div className="data-aggregator-item" />
+              </div>
+            </div>
+            <div className="frame-wrapper1">
+              <div className="punkbuster-parent">
+                <div className="punkbuster">punkbuster</div>
+                <div className="on-parent">
+                  <div className="on1">on</div>
+                  <div className="only-squad-leader-spawn-parent">
+                    <div className="only-squad-leader">
+                      only squad leader spawn
+                    </div>
+                    <div className="off-wrapper">
+                      <div className="off">off</div>
+                    </div>
+                    <div className="vehicle-spawn-delay">
+                      vehicle spawn delay
+                    </div>
+                  </div>
+                  <div className="div3">25</div>
+                </div>
+              </div>
+            </div>
+            <div className="line-parent">
+              <div className="frame-child" />
+              <div className="frame-item" />
+              <div className="frame-inner" />
+            </div>
+            <div className="frame-wrapper2">
+              <div className="frame-parent10">
+                <div className="fairfight-wrapper">
+                  <div className="fairfight">fairfight</div>
+                </div>
+                <div className="on-group">
+                  <div className="on2">on</div>
+                  <div className="vehicles">vehicles</div>
+                </div>
+                <div className="on-container">
+                  <div className="on3">on</div>
+                  <div className="bullet-damage">bullet damage</div>
+                </div>
+                <div className="div4">100</div>
+              </div>
+            </div>
+            <div className="line-group">
+              <div className="line-div" />
+              <div className="frame-child1" />
+              <div className="frame-child2" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flow-controllers">
+        <div className="password-parent">
+          <div className="password">password</div>
+          <div className="star-shape1">
+            <div className="off-parent">
+              <div className="off1">off</div>
+              <div className="team-balance">team balance</div>
+            </div>
+            <div className="on-parent1">
+              <div className="on4">on</div>
+              <div className="kick-after-team">kick after team kills</div>
+            </div>
+            <div className="div5">5</div>
+          </div>
+        </div>
+      </div>
+      <div className="complex-connectors">
+        <div className="line-container">
+          <div className="frame-child3" />
+          <div className="frame-child4" />
+          <div className="frame-child5" />
+        </div>
+      </div>
+      <div className="tree-splitters">
+        <div className="stack-layout-parent">
+          <div className="stack-layout">
+            <div className="preset">preset</div>
+          </div>
+          <div className="indentation-guide">
+            <div className="normal">normal</div>
+            <div className="minimap-spotting">minimap spotting</div>
+          </div>
+          <div className="indentation-guide1">
+            <div className="on5">on</div>
+            <div className="player-health">player health</div>
+          </div>
+          <div className="div6">100</div>
+        </div>
+      </div>
+      <div className="flow-splitting">
+        <div className="border-styles">
+          <div className="drop-shadow-blur">
+            <div className="drop-shadow-blur-child" />
+            <div className="drop-shadow-blur-item" />
+            <div className="drop-shadow-blur-inner" />
+          </div>
+          <div className="background-color">
+            <div className="gradient-fill">
+              <div className="hud">hud</div>
+              <div className="on-parent2">
+                <div className="on6">on</div>
+                <div className="sequence-flow">
+                  <div className="player-respawn-time">player respawn time</div>
+                </div>
+                <div className="div7">100</div>
+              </div>
+            </div>
+          </div>
+          <div className="background-color1">
+            <div className="background-color-child" />
+            <div className="background-color-item" />
+          </div>
+          <div className="background-color2">
+            <div className="frame-parent11">
+              <div className="p-vehicle-cam-wrapper">
+                <div className="p-vehicle-cam">3p vehicle cam</div>
+              </div>
+              <div className="on-parent3">
+                <div className="on7">on</div>
+                <div className="kick-after-idle">kick after idle</div>
+              </div>
+              <div className="div8">300</div>
+            </div>
+          </div>
+          <div className="line-parent1">
+            <div className="frame-child6" />
+            <div className="frame-child7" />
+          </div>
+          <div className="border-styles-inner">
+            <div className="regenerative-health-parent">
+              <div className="regenerative-health">regenerative health</div>
+              <div className="on-parent4">
+                <div className="on8">on</div>
+                <div className="ban-after-kicks">ban after kicks</div>
+              </div>
+              <div className="div9">3</div>
+            </div>
+          </div>
+          <div className="line-parent2">
+            <div className="frame-child8" />
+            <div className="frame-child9" />
+          </div>
+          <div className="border-styles-child">
+            <div className="kill-cam-parent">
+              <div className="kill-cam">kill cam</div>
+              <div className="on9">on</div>
+            </div>
+          </div>
+          <div className="background-color3">
+            <div className="background-color-inner" />
+          </div>
+          <div className="output-filters">
+            <div className="friendly-fire-parent">
+              <div className="friendly-fire">friendly fire</div>
+              <div className="off2">off</div>
+            </div>
+          </div>
+          <div className="border-styles-inner1">
+            <div className="frame-child10" />
+          </div>
+          <div className="output-filters1">
+            <div className="d-spotting-parent">
+              <div className="d-spotting">3d spotting</div>
+              <div className="on10">on</div>
+            </div>
+          </div>
+          <div className="error-handlers">
+            <div className="error-handlers-child" />
+          </div>
+          <div className="border-styles-inner2">
+            <div className="enemy-name-tags-parent">
+              <div className="enemy-name-tags">enemy name tags</div>
+              <div className="on11">on</div>
+            </div>
+          </div>
+          <div className="border-styles-inner3">
+            <div className="frame-child11" />
+          </div>
+        </div>
+      </div>
+      <main className="menu-darker-bg-parent">
+        <section className="menu-darker-bg" />
+        <img
+          className="germany-flag-icon"
+          loading="lazy"
+          alt=""
+          src="/germanyflag.svg"
+        />
+      </main>
+      <FrameComponent />
+    </div>
+  );
+};
+
+export default MenuMultiplayerServerBrow;
